f7ab1ae98c464a36429071856e508d78eec2eea9
Negar <nasirinegar13@gmail.com>
2023-01-23 09:51:54 +0300

    installed react spring

--- src/components/animatedDiv/animatedDiv.jsx
+++ src/components/animatedDiv/animatedDiv.jsx
@@ -1,30 +1,28 @@
 import React from "react";
-import { useSpring, animated, to } from "@react-spring/web";
+import { useSpring, animated } from "@react-spring/web";
 import { useDrag } from "@use-gesture/react";
+import { to } from "react-spring";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 function AnimatedDiv({ children }) {
-  const [{ x, bg }, set] = useSpring(() => ({
+  const [{ x }, set] = useSpring(() => ({
     x: 0,
   }));
+
   const bind = useDrag(({ delta, down, last }) => {
     const x = down ? delta[0] : 0;
     set({ x });
 
-    if (last && delta[0] && delta[0] > 10) {
+    if (last && delta[0] && delta[0] > 100) {
       onEnd && onEnd();
     }
   });
+  const transform = to([x], (x) => `translate3d(${x}px,0,0)`);
 
   return (
     <animated.div
       {...bind()}
-      style={{
-        transform: x.to({
-          range: [0, 0.2, 0.5, 1],
-          output: [1, 0.7, 1.5, 1],
-        }),
-      }}
+      style={{ transform }}
     >
       {children}
     </animated.div>
