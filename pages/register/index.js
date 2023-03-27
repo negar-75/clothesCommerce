235c3866a675c292cf5674be26392ae8426243c2
Negar <nasirinegar13@gmail.com>
2023-03-27 23:59:50 +0100

    added mode key

--- pages/register/index.js
+++ pages/register/index.js
@@ -28,6 +28,7 @@ function Register() {
     event.preventDefault();
     console.log(user);
     fetch("http://localhost:3000/api/user", {
+      mode: "no-cors",
       method: "POST",
       body: JSON.stringify(user),
       headers: {
