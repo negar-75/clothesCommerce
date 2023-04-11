78c4ca9644374cdc7fad63aef9683555b18fd94d
Negar <nasirinegar13@gmail.com>
2023-04-11 11:18:15 +0100

    changed svg

--- pages/index.js
+++ pages/index.js
@@ -9,46 +9,11 @@ import RedBanner from "../src/components/redBanner/redBanner";
 import CategoriesSlideShow from "../src/components/categoriesSlideShow/categoriesSlideShow";
 import Footer from "../src/components/footer/footer";
 import { useMediaQuery } from "react-responsive";
+import SVG from "../src/components/svg/svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ data, store, categories, footerNavigation }) {
-  const svgRef = useRef(null);
-  const pathRef = useRef(null);
-  let pathLength = null;
-  const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 1224px)",
-  });
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
-  useEffect(() => {
-    // Get the path element and its total length
-    const path = pathRef.current;
-    pathLength = path.getTotalLength();
-    // Set the initial stroke dash array and offset values
-    path.style.strokeDasharray = pathLength + " " + pathLength;
-    path.style.strokeDashoffset = pathLength;
-
-    // Set the reference to the path length
-    svgRef.current = pathLength;
-
-    // Add event listener to detect scrolling
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      // Remove event listener on unmount
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  const handleScroll = () => {
-    // Calculate scroll percentage and stroke dash offset
-    const scrollPercent =
-      (document.documentElement.scrollTop + document.body.scrollTop) /
-      (document.documentElement.scrollHeight -
-        document.documentElement.clientHeight);
-    const draw = pathLength * scrollPercent;
-    pathRef.current.style.strokeDashoffset = pathLength - draw;
-  };
-
   return (
     <div className="relative h-full">
       <Head>
@@ -66,43 +31,11 @@ export default function Home({ data, store, categories, footerNavigation }) {
           href="/favicon.ico"
         />
       </Head>
-      <div className="line-container w-full h-[100%] text-left absolute z-[-100] ">
-        {isDesktopOrLaptop && (
-          <svg
-            viewBox="0 0 823 2564"
-            fill="none"
-            preserveAspectRatio="xMidYMid meet"
-            height="2500"
-            className="inline-block h-[100%] w-[100%] mt-[5%]"
-            ref={svgRef}
-          >
-            <path
-              d="M80 2C75.6283 16.5722 57.7662 27.3465 48 38.4444C24.9411 64.6477 11.6937 91.3807 4.44444 125.556C-15.5513 219.821 86.5405 263.976 159.111 294.444C210.179 315.885 322.717 348.487 335.111 413.556C345.175 466.391 310.736 517.899 286.222 561.111C244.174 635.231 208.989 723.107 216.889 810C228.217 934.609 381.573 1043.45 465.778 1120.22C544.604 1192.09 675.433 1292.51 654.667 1417.11C641.335 1497.1 534.7 1525.99 516.889 1609.11C496.446 1704.51 614.539 1777.31 665.778 1842C730.46 1923.67 784.575 2001 812 2101.56C826.028 2152.99 828.106 2185.57 777.778 2214.44C729.236 2242.3 671.658 2256.51 620 2277.56C494.008 2328.89 392.575 2445.77 328 2562"
-              stroke="black"
-              ref={pathRef}
-              height={"100%"}
-            />
-          </svg>
-        )}
-        {isTabletOrMobile && (
-          <svg
-            viewBox="0 0 279 5522"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="xMidYMid meet"
-            className="inline-block h-[100%] w-[100%]  "
-            ref={svgRef}
-          >
-            <path
-              d="M1 1C316.542 1 180.781 308.452 100.556 472.111C37.1211 601.517 55.5897 728.3 160.111 824.111C253.851 910.039 209 1095.12 209 1208.11C209 1342.64 202.748 1479.03 209 1613.44C211.144 1659.54 244.679 1712.25 264.111 1753.89C305.738 1843.09 242.304 1922.03 201.889 1994.78C138.842 2108.26 47.9928 2283.16 113 2417C171.018 2536.45 229.486 2615.24 225 2756.56C221.36 2871.2 132.343 2949.95 106.778 3057.89C89.5271 3130.73 90.3581 3222.58 97.8889 3297.89C105.798 3376.98 168.076 3449.89 205.444 3517.44C236.495 3573.57 225 3647.58 225 3709.44C225 3782.51 228.959 3853.26 199.222 3921.89C169.964 3989.41 107.054 4070.79 97.8889 4144.11C81.4706 4275.46 108.364 4392.24 177 4505C319.77 4739.55 107.63 4972.84 160.111 5209C172.43 5264.44 209 5322.78 209 5377C209 5427.34 185.932 5521 257 5521"
-              stroke="black"
-              stroke-linecap="round"
-              ref={pathRef}
-              height={"100%"}
-            />
-          </svg>
-        )}
-      </div>
+
+      <SVG
+        mobilePath="M131.048 0C69.7495 6.89776 63.2429 110.075 52.6064 154.702C28.5541 255.617 49.7799 373.773 80.4739 469.854C118.064 587.523 193.554 696.822 243.034 809.571C307.653 956.815 316.831 1134.1 316.831 1293.01C316.831 1426.17 322.268 1548.95 271.418 1674.02C218.412 1804.39 165.66 1938.04 117.631 2070.7C83.083 2166.12 84.6024 2262.16 84.6024 2361.81C84.6024 2493.25 162.013 2611.29 191.428 2737.07C212.425 2826.86 251.807 2908.71 251.807 3001C251.807 3125.33 229.968 3224.38 165.108 3331.31C123.172 3400.45 84.6024 3458.46 84.6024 3539.32C84.6024 3622.09 75.3132 3703.98 75.3132 3787.06C75.3132 3872.7 96.1192 3952.95 114.534 4035.83C141.855 4158.81 187.683 4276.93 214.651 4400.64C239.727 4515.66 223.94 4644.92 223.94 4761.26C223.94 4830.72 224.567 4898.28 213.618 4967.18C182.094 5165.57 100.445 5322.2 1 5494"
+        desktopPath="M55.8986 1.14862C28.5208 1.14862 1 28.6146 1 56.2819C1 80.5167 2.29292 88.8833 15.7804 108.365C27.1049 124.723 42.6015 138.128 53.5526 155.053C63.4129 170.291 64.3446 176.868 64.3446 195.405C64.3446 216.646 51.6757 229.713 51.6757 250.304C51.6757 277.658 52.5965 292.032 65.7523 315.995C82.8552 347.147 116.287 367.805 148.804 380.278C164.432 386.272 199.576 391.714 203.468 413.123C207.179 433.531 203.758 457.534 197.368 476.702C175.433 542.507 224.866 604.236 280.185 636.706C352.595 679.208 448.635 718.096 448.635 814.54C448.635 850.809 436.528 885.052 440.658 922.226C445.127 962.439 490.508 1001.5 523.006 1021C607.432 1071.65 727.677 1123.2 747.528 1232.38C754.429 1270.34 744.243 1311.44 744.243 1349.68C744.243 1373.37 740.925 1398.2 750.812 1420.3C761.348 1443.85 775.18 1459.08 790.696 1479.19C815.739 1511.65 850.994 1539.54 870.229 1576.08C891.948 1617.35 896.27 1673.69 896.27 1719.43C896.27 1759.32 868.489 1772.43 845.829 1798.49C812.059 1837.33 801.214 1884.61 792.573 1934.1C785.118 1976.79 782.966 2025.4 763.012 2064.31C745.994 2097.49 718.713 2120.2 687.937 2140.32C653.986 2162.52 638.964 2181.86 617.554 2215.86C600.049 2243.67 597.877 2268.37 591.278 2299.38C588.12 2314.22 582.861 2356.87 570.163 2366.01C559.141 2373.95 538.251 2390.64 524.649 2391.35C489.954 2393.18 503.353 2446.25 469.75 2446.25"
+      />
       <header>
         <Navbar />
         <HeaderContainer data={data} />
@@ -144,3 +77,31 @@ export async function getStaticProps() {
     },
   };
 }
+
+<svg
+  width="255"
+  height="5430"
+  viewBox="0 0 255 5430"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d=""
+    stroke="#555050"
+    stroke-linecap="round"
+  />
+</svg>;
+
+<svg
+  width="318"
+  height="5495"
+  viewBox="0 0 318 5495"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M131.048 0C69.7495 6.89776 63.2429 110.075 52.6064 154.702C28.5541 255.617 49.7799 373.773 80.4739 469.854C118.064 587.523 193.554 696.822 243.034 809.571C307.653 956.815 316.831 1134.1 316.831 1293.01C316.831 1426.17 322.268 1548.95 271.418 1674.02C218.412 1804.39 165.66 1938.04 117.631 2070.7C83.083 2166.12 84.6024 2262.16 84.6024 2361.81C84.6024 2493.25 162.013 2611.29 191.428 2737.07C212.425 2826.86 251.807 2908.71 251.807 3001C251.807 3125.33 229.968 3224.38 165.108 3331.31C123.172 3400.45 84.6024 3458.46 84.6024 3539.32C84.6024 3622.09 75.3132 3703.98 75.3132 3787.06C75.3132 3872.7 96.1192 3952.95 114.534 4035.83C141.855 4158.81 187.683 4276.93 214.651 4400.64C239.727 4515.66 223.94 4644.92 223.94 4761.26C223.94 4830.72 224.567 4898.28 213.618 4967.18C182.094 5165.57 100.445 5322.2 1 5494"
+    stroke="#808080"
+    stroke-linecap="round"
+  />
+</svg>;
---
+++ src/components/svg/svg.jsx
@@ -0,0 +1,77 @@
+import { useState, useRef, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+
+function SVG({ desktopPath, mobilePath }) {
+  const svgRef = useRef(null);
+  const pathRef = useRef(null);
+  const [pathLength, setPathLength] = useState(null);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  let path;
+  let length;
+  const handleScroll = () => {
+    const scrollPercent =
+      window.pageYOffset /
+      (document.documentElement.scrollHeight -
+        document.documentElement.clientHeight);
+
+    const draw = length * scrollPercent * 1.001;
+
+    path.style.strokeDashoffset = length - draw;
+  };
+
+  useEffect(() => {
+    path = pathRef.current;
+    length = path.getTotalLength();
+    setPathLength(length);
+    path.style.strokeDasharray = `${length} `;
+    path.style.strokeDashoffset = length;
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="line-container w-full h-[100%] text-left absolute z-[-100] ">
+      {isDesktopOrLaptop && (
+        <svg
+          viewBox="0 0 823 2564"
+          fill="none"
+          preserveAspectRatio="xMidYMid meet"
+          className="inline-block h-[100%] w-[100%] mt-[0%] overflow-visible"
+          ref={svgRef}
+        >
+          <path
+            d={desktopPath}
+            stroke="#a5a5a5"
+            ref={pathRef}
+          />
+        </svg>
+      )}
+      {isTabletOrMobile && (
+        <svg
+          viewBox="0 0 318 5495"
+          fill="none"
+          preserveAspectRatio="xMidYMid meet"
+          className="inline-block h-[100%] w-[100%] overflow-visible "
+          ref={svgRef}
+        >
+          <path
+            d={mobilePath}
+            stroke="#a5a5a5"
+            ref={pathRef}
+            strokeLinecap="round"
+          />
+        </svg>
+      )}
+    </div>
+  );
+}
+
+export default SVG;
