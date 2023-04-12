cc576a74d54fec63e5aae059021ab55a181230aa
Negar <nasirinegar13@gmail.com>
2023-04-12 18:04:29 +0100

    added totalPrice to session

--- pages/cart/checkout.js
+++ pages/cart/checkout.js
@@ -1,13 +1,29 @@
 import Payment from "../../src/components/payment/payment";
 import ContactInfo from "../../src/components/contactInfo/contactInfo";
+import { withIronSessionSsr } from "iron-session/next";
 
-function Checkout() {
+function Checkout({ totalPrice }) {
   return (
     <div className="flex flex-col lg:flex-row h-full">
-      <ContactInfo />
+      <ContactInfo price={totalPrice} />
       <Payment />
     </div>
   );
 }
 
 export default Checkout;
+
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req }) {
+    const totalPrice = req.session.totalPrice;
+
+    return { props: { totalPrice } };
+  },
+  {
+    password: process.env.SESSION_SECRET,
+    cookieName: "session",
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  }
+);
