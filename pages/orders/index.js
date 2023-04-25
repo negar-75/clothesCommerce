8fed073f6f60d0d485f6620c7a346ce43278f80a
Negar <nasirinegar13@gmail.com>
2023-04-25 16:23:20 +0100

    added h3

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -20,56 +20,64 @@ function Orders({ data, id }) {
         </p>
       </div>
 
-      <div className="divide-y-2 px-5 w-[100%] ">
-        <div className="flex md:divide-x-2 gap-4 divide-stone-300 md:mb-10 flex-col md:flex-row mb-6">
-          <p>
-            <span className="text-stone-500">Order Date : </span>
-            {data.information.date}
-          </p>
+      {data.cart.length === 0 ? (
+        <div className="text-center">
+          <h3>You have not ordered anything yet</h3>
+        </div>
+      ) : (
+        <>
+          <div className="divide-y-2 px-5 w-[100%] ">
+            <div className="flex md:divide-x-2 gap-4 divide-stone-300 md:mb-10 flex-col md:flex-row mb-6">
+              <p>
+                <span className="text-stone-500">Order Date : </span>
+                {data.information.date}
+              </p>
 
-          <p className="text-green-800">
-            <FontAwesomeIcon
-              icon={faTruckFast}
-              beatFade
-              className="md:mx-2 text-green-700 mr-2"
-            />
-            Estimated delivery :{" "}
-            {deliveryDate.toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}{" "}
-          </p>
-        </div>
-        <div className="flex overflow-x-scroll py-3 ">
-          {data.cart.map((item) => (
-            <div className="flex items-center md:gap-4 md:p-3  gap-2 md:flex-[0_0_18%] flex-[0_0_75%]  mr-1 ">
-              <img
-                src={item.image}
-                className="w-[160px] h-[230px] object-cover rounded-md border-2 p-1 border-stone-500 "
-              />
-              <div className="flex flex-col items-baseline  text-lg">
-                <span className="font-semibold">{item.title}</span>
-                <span className="align-middle">
-                  <span className="text-[10px]">✗</span>
-                  {item.quantity}
-                </span>
-                <span>{item.price * item.quantity}$</span>
-              </div>
+              <p className="text-green-800">
+                <FontAwesomeIcon
+                  icon={faTruckFast}
+                  beatFade
+                  className="md:mx-2 text-green-700 mr-2"
+                />
+                Estimated delivery :{" "}
+                {deliveryDate.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}{" "}
+              </p>
             </div>
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 px-5">
-        <h4>Deliver to :</h4>
-        <span>
-          {data.information.firstName.toUpperCase() +
-            "  " +
-            data.information.lastName.toUpperCase()}
-        </span>
-        <span>{data.information.adress.toUpperCase()} </span>
-        <span>{data.information.phone}</span>
-      </div>
+            <div className="flex overflow-x-scroll py-3 ">
+              {data.cart.map((item) => (
+                <div className="flex items-center md:gap-4 md:p-3  gap-2 md:flex-[0_0_18%] flex-[0_0_75%]  mr-1 ">
+                  <img
+                    src={item.image}
+                    className="w-[160px] h-[230px] object-cover rounded-md border-2 p-1 border-stone-500 "
+                  />
+                  <div className="flex flex-col items-baseline  text-lg">
+                    <span className="font-semibold">{item.title}</span>
+                    <span className="align-middle">
+                      <span className="text-[10px]">✗</span>
+                      {item.quantity}
+                    </span>
+                    <span>{item.price * item.quantity}$</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 px-5">
+            <h4>Deliver to :</h4>
+            <span>
+              {data.information.firstName.toUpperCase() +
+                "  " +
+                data.information.lastName.toUpperCase()}
+            </span>
+            <span>{data.information.adress.toUpperCase()} </span>
+            <span>{data.information.phone}</span>
+          </div>
+        </>
+      )}
     </Layout>
   );
 }
