49d8f540d29a4e05029f8d81b5d9558c5e3391d3
Negar <nasirinegar13@gmail.com>
2023-05-02 18:22:52 +0100

    added style  property

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -8,7 +8,16 @@ function SignInPage(props) {
   const passwordRef = useRef();
 
   return (
-    <div className="bg-amber-50 h-full flex flex-col items-center py-10">
+    <div
+      className=" h-full flex flex-col py-10  
+     "
+      style={{
+        background:
+          "linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url(/bc-image.jpg)",
+        backgroundPosition: "top",
+        backgroundSize: "cover",
+      }}
+    >
       <div className="w-full bg-stone-200 mt-10 px-5 py-1 md:px-16 mb-10">
         <p className="text-xs">
           <Link href="/">
@@ -17,74 +26,81 @@ function SignInPage(props) {
           Sign in
         </p>
       </div>
-      <h2 className="mb-10">Sign in</h2>
-      <form
-        className="items-center  flex flex-col gap-6 shadow-md bg-white py-10 px-8 lg:px-20 rounded-lg mx-3"
-        action=""
-      >
-        <input
-          name="csrfToken"
-          type="hidden"
-          defaultValue={props.csrfToken}
-        />
-        <div className="flex gap-2 flex-col lg:w-[300px] w-[95%]">
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            onChange={(e) => (emailRef.current = e.target.value)}
-          />
-        </div>
-        <div className="flex gap-2 flex-col lg:w-[300px] w-[95%]">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => (passwordRef.current = e.target.value)}
-          />
-        </div>
-        <button
-          className="border-2 w-[200px] h-[40px] uppercase"
-          onClick={(e) => {
-            console.log(process.env.NEXTAUTH_URL);
-            e.preventDefault();
-            signIn("credentials", {
-              email: emailRef.current,
-              password: passwordRef.current,
-              // callbackUrl: `,
-            });
-          }}
+      <h2 className="mb-10 md:text-center ml-8">Sign in</h2>
+      <div className=" flex flex-col items-center justify-center">
+        <form
+          className="items-center flex flex-col gap-6 bg-white py-10 px-8 lg:px-20 rounded-lg lg:w-[30%] w-[90%] "
+          action=""
         >
-          Sign in
-        </button>
-      </form>
+          <input
+            name="csrfToken"
+            type="hidden"
+            defaultValue={props.csrfToken}
+          />
+          <div className="flex gap-2 flex-col lg:w-[300px] w-[95%]">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              onChange={(e) => (emailRef.current = e.target.value)}
+            />
+          </div>
+          <div className="flex gap-2 flex-col lg:w-[300px] w-[95%]">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => (passwordRef.current = e.target.value)}
+            />
+          </div>
+          <button
+            className="border-2 w-[200px] h-[40px] uppercase"
+            onClick={(e) => {
+              console.log(process.env.NEXTAUTH_URL);
+              e.preventDefault();
+              signIn("credentials", {
+                email: emailRef.current,
+                password: passwordRef.current,
+                // callbackUrl: `,
+              });
+            }}
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+
       <div
-        className="items-center mt-10 flex flex-col gap-4 pb-10
+        className="items-center mt-10 flex flex-col gap-4 pb-10 
       "
       >
-        <h3>Or Sign in with:</h3>
+        <div className="font-semibold mb-8 relative flex items-center w-[80%]">
+          <div className="flex-grow h-[1px] bg-black "></div>
+          <h3 className="flex-shrink text-2xl  px-4 inline">OR</h3>
+          <div className="flex-grow h-[1px] bg-black "></div>
+        </div>
         <button
-          className="flex items-center border-2 w-[200px] justify-center gap-2 h-[40px]"
+          className="flex items-center border-2 w-[200px] justify-center gap-2 h-[40px] border-black"
           onClick={() => signIn("google")}
         >
           <img
             src="/google_icon.png"
-            className="w-[40px] h-[40px]"
+            className="w-[40px] h-[40px] object-cover"
           />
 
           <span>Google</span>
         </button>
-        <button className="flex items-center border-2 w-[200px] justify-center gap-2 h-[40px]">
+        <button className="flex items-center border-2 w-[200px] justify-center gap-2 h-[40px] border-black">
           <img
             src="/facebook_icon.png"
-            className="w-[30px] h-[30px] ml-2"
+            className="w-[30px] h-[30px] object-cover"
           />
           <span className="ml-1">Facebook</span>
         </button>
-        <button className="flex items-center w-[200px] justify-center gap-2 h-[40px] border-2">
+        <button className="flex items-center w-[200px] justify-center gap-2 h-[40px] border-2 border-black">
           <img
             src="/apple_icon.png"
-            className="w-[30px] h-[30px]"
+            className="w-[30px] h-[30px] object-cover"
           />
           <span>Apple</span>
         </button>
